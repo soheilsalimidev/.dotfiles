cc44641b7dbb0bc372fa32afacaaf78dc0c40930
Soheil Salimi <soheilsalimidev@gmail.com>
2024-09-29 21:45:21 +0330

    update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,70 +1,69 @@
-local wakatime = require('plugins.wakatime')
+local wakatime = require("plugins.wakatime")
 
 local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
+	blue = "#80a0ff",
+	cyan = "#79dac8",
+	black = "#080808",
+	white = "#c6c6c6",
+	red = "#ff5189",
+	violet = "#d183e8",
+	grey = "#303030",
 }
 
 local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.white },
-  },
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.white },
+	},
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
 
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.white },
-  },
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
+	},
 }
 
 local clients_lsp = function()
-  local bufnr = vim.api.nvim_get_current_buf()
+	local bufnr = vim.api.nvim_get_current_buf()
 
-  local clients = vim.lsp.buf_get_clients(bufnr)
-  if next(clients) == nil then
-    return ''
-  end
+	local clients = vim.lsp.buf_get_clients(bufnr)
+	if next(clients) == nil then
+		return ""
+	end
 
-  local c = {}
-  for _, client in pairs(clients) do
-    table.insert(c, client.name)
-  end
-  return '\u{f085} [ ' .. table.concat(c, '|') .. ' ]'
+	local c = {}
+	for _, client in pairs(clients) do
+		table.insert(c, client.name)
+	end
+	return "\u{f085} [ " .. table.concat(c, "|") .. " ]"
 end
 
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-    theme = bubbles_theme,
-  },
-  sections = {
-    lualine_a = { 'mode', },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { clients_lsp },
-    lualine_y = { wakatime.get_wakatime },
-    lualine_z = { 'location' }
-  },
-  extensions = { 'nvim-tree' }
-
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
+		theme = bubbles_theme,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics", require("recorder").recordingStatus },
+		lualine_c = { "filename" },
+		lualine_x = { clients_lsp },
+		lualine_y = { wakatime.get_wakatime },
+		lualine_z = { "location" },
+	},
+	extensions = { "nvim-tree" },
+})
 
 vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
 vim.api.nvim_create_autocmd("User", {
-  group = "lualine_augroup",
-  pattern = "LspProgressStatusUpdated",
-  callback = require("lualine").refresh,
+	group = "lualine_augroup",
+	pattern = "LspProgressStatusUpdated",
+	callback = require("lualine").refresh,
 })
